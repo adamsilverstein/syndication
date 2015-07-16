1ef8f02b3cb24d95f7e286cff2c0bd4811bc514a
Adam Silverstein <adam@10up.com>
2015-07-16 12:24:23 -0600

    Remove settings management from settings screen class

--- includes-new/admin/class-settings-screen.php
+++ includes-new/admin/class-settings-screen.php
@@ -9,30 +9,11 @@ namespace Automattic\Syndication\Admin;
 
 class Settings_Screen {
 
-	protected $push_syndicate_default_settings;
-
-	protected $push_syndicate_settings;
 
 	public function __construct() {
 
-		add_action( 'init', array( $this, 'init' ) );
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
 		add_action( 'admin_menu', array( $this, 'register_syndicate_settings' ) );
-	}
-
-	public function init() {
-
-		$this->push_syndicate_default_settings = array(
-			'selected_pull_sitegroups'  => array(),
-			'selected_post_types'       => array( 'post' ),
-			'delete_pushed_posts'       => 'off',
-			'pull_time_interval'        => '3600',
-			'update_pulled_posts'       => 'off',
-			'client_id'                 => '',
-			'client_secret'             => ''
-		);
-
-		$this->push_syndicate_settings = wp_parse_args( (array) get_option( 'push_syndicate_settings' ), $this->push_syndicate_default_settings );
 	}
 
 	public function admin_init() {
